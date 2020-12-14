4df231370deb47ce6d854f8183c90dcf5d78aa12
liqibo <1289758842@qq.com>
2020-12-14 16:38:22 +0800

    init

---
+++ src/main/java/com/atguigu/Demo/jianfan.java
@@ -0,0 +1,10 @@
+package com.atguigu.Demo;
+
+public class jianfan {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+        System.out.println("2222222");
+        System.out.println("3333333");
+        System.out.println("4444444");
+    }
+}
