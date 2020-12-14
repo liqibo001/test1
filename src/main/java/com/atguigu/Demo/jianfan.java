963d71beacd57991ea6d997321ea871c7886c665
liqibo <1289758842@qq.com>
2020-12-14 16:42:59 +0800

    init

--- src/main/java/com/atguigu/Demo/jianfan.java
+++ src/main/java/com/atguigu/Demo/jianfan.java
@@ -5,6 +5,8 @@ public class jianfan {
         System.out.println("1111111");
         System.out.println("2222222");
         System.out.println("3333333");
+        System.out.println("3333333");
         System.out.println("4444444");
+
     }
 }
